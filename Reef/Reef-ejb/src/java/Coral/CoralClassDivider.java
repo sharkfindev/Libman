122863e9e3d123c740bdf6d5fc04f5090fbe378c
sharkfindev <s.kumar@sharkfinco.com>
2014-03-19 12:51:59 -0700

    getter and setters complete

---
+++ Reef/Reef-ejb/src/java/Coral/CoralClassDivider.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Coral;
+
+import javax.swing.JTable;
+
+/**
+ *
+ * @author sujay
+ */
+public class CoralClassDivider {
+ CoralClassDivider(){
+     
+ }   
+ //getters
+ String[][][][][][]getbusinessinfo(){
+     return null;
+     
+ }
+ String[][][][][][][][][][][][][][][][]getStudents(){
+     return null;
+     
+ }
+  String[][][][][][]getItems(){
+     return null;
+     
+  }
+    
+ //setters
+  String[][][][][][]setbusinessinfo(){
+     return null;
+     
+ }
+    String[][][][][][][][][][][][][][][][] setStudents(){
+     return null;
+     
+ }
+    String[][][][][][] setItems(){
+     return null;
+     
+ }
+}
