9cce19f6abab091a2f12c83bf56571d72ddc8914
sharkfindev <s.kumar@sharkfinco.com>
2014-03-20 09:14:46 -0700

    get student getters and persistance class and xml need setters for student as well as entity for general and items with id and getters and setters

--- Reef/Reef-ejb/src/java/Coral/CoralClassDivider.java
+++ Reef/Reef-ejb/src/java/Coral/CoralClassDivider.java
@@ -5,7 +5,10 @@
  */
 
 package Coral;
-import javax.swing.JTable;
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 /**
  *
@@ -16,32 +19,102 @@ public class CoralClassDivider {
      
  }   
  //getters
- String[][][][][][]getbusinessinfo(){
- 
-     return null;
-     
-     
- }
- String[][][][][][][][][][][][][][][][]getStudents(){
-     return null;
-     
- }
-  String[][][][][][]getItems(){
-     return null;
-     
-  }
+ @Entity
+@Table(name="WEB_BOOKSTORE_BOOKS")
+
+public class Student implements Serializable {
+
+    private String StudentID;
+    private String LastName;
+    private String FirstName;
+    private String email;
+    private String PhoneNumber;
+    private String DaysAttending;
+    private String Comments;
+    private String ItemsCheckedCurrently;
+    private String ItemsChecked;
+    private String Messages;
+    private String Timeslot;
+    private String Vacation;
+    private String Sick;
+    private String Flagged;
+    private String Attended;
+    private String Absent;
+
+    public Student() { }
+
+    public Student(String StudentID, String LastName, String FirstName,String email,String PhoneNumber,String DaysAttending,String Comments,String ItemsCheckedCurrently,String ItemsChecked,String Timeslot,String Vacation,String Messages,String Sick,String Flagged,String Attended,String Absent) {
+        this.StudentID = StudentID;
+        this.LastName = LastName;
+        this.FirstName=FirstName;
+        this.PhoneNumber=PhoneNumber;
+        this.email=email;
+        this.Messages=Messages;
+        this.DaysAttending=DaysAttending;
+        this.Comments=Comments;
+        this.ItemsCheckedCurrently=ItemsCheckedCurrently;
+        this.ItemsChecked=ItemsChecked;
+        this.Timeslot=Timeslot;
+        this.Vacation=Vacation;
+        this.Sick=Sick;
+        this.Flagged=Flagged;
+        this.Attended=Attended;
+        this.Absent=Absent;
+  
+    }
+     @Id
+    public String getStudentID() {
+        return this.StudentID;
+    }
+
+    public String getLastName() {
+        return this.LastName;
+    }
+      public String getFirstName() {
+        return this.FirstName;
+    }
+
+    public String getPhoneNumber() {
+        return this.PhoneNumber;
+    }
+      public String getemail() {
+        return this.email;
+    }
+
+    public String getMessages() {
+        return this.Messages;
+    }
+      public String getDaysAttending() {
+        return this.DaysAttending;
+    }
+
+    public String getComments() {
+        return this.Comments;
+    }
+      public String getItemsCheckedCurrently() {
+        return this.ItemsCheckedCurrently;
+    }
+
+    public String getItemsChecked() {
+        return this.ItemsChecked;
+    }
+      public String getVacation() {
+        return this.Vacation;
+    }
+
+    public String getSick() {
+        return this.Sick;
+    }
+      public String getFlagged() {
+        return this.Flagged;
+    }
+
+    public String getAttended() {
+        return this.Attended;
+    }
+    public String getAbsent() {
+        return this.Absent;
+    }
     
- //setters
-  String[][][][][][]setbusinessinfo(){
-     return null;
-     
- }
-    String[][][][][][][][][][][][][][][][] setStudents(){
-     return null;
-     
- }
-    String[][][][][][] setItems(){
-     return null;
-     
  }
 }
